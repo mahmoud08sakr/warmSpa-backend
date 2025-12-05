3b8b96846b80fa79ec4df2d8efa7985ea576c0f1
mahmoud08sakr <mahmoud23mostafa@gmail.com>
2025-12-05 02:31:01 +0200

    solve the error

--- src/modules/branch/branch.router.js
+++ src/modules/branch/branch.router.js
@@ -19,7 +19,6 @@ const router = express.Router();
 
 router.get('/within/distance', getBranchesWithinHandler);
 router.get('/', getAllBranchesHandler);
-router.get('/get-user-data' , getUserData)
 router.get('/city' ,getAllBranchesCityHandler)
 router.get('/get-branch-by-id/:id', getBranchHandler);
 router.use(auth);
