cf37cd17fa984de3b9b2d9a1a4990166382437f9
mahmoud08sakr <mahmoud23mostafa@gmail.com>
2025-12-12 00:09:22 +0200

    add the session id api

--- src/modules/order/order.controller.js
+++ src/modules/order/order.controller.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createPaymentIntent, getOrder, getUserOrders, getAllOrderForAdmin } from "./order.service.js";
+import { createPaymentIntent, getOrder, getOrderBySessionId, getOrderByPaymentIntent, getUserOrders, getAllOrderForAdmin } from "./order.service.js";
 import { auth } from "../../midlleware/auth.js";
 import { checkRole } from "../../midlleware/role.js";
 
@@ -7,6 +7,8 @@ const router = Router()
 
 router.post('/create-payment-intent/:branchId/:serviceId', auth, createPaymentIntent);
 router.get('/get-order-by-id/:id', auth, getOrder);
+router.get('/get-order-by-session/:sessionId', getOrderBySessionId);
+router.get('/get-order-by-payment-intent/:paymentIntentId', getOrderByPaymentIntent);
 router.get('/user/:userId', auth, getUserOrders);
 router.get('/get-all-orders-by-admin', auth, checkRole("Admin", "SAdmin", "Branch"), getAllOrderForAdmin);
 
